5186611d11437150ea5526f7eecd60ef80b87df7
ea-oconnor <oconnor.eamon0@gmail.com>
2024-09-29 12:20:48 -0400

    Updated modal image

--- assets/js/main.js
+++ assets/js/main.js
@@ -171,13 +171,14 @@
 		var modal = document.getElementById("myModal");
 
 		// Get the image and insert it inside the modal - use its "alt" text as a caption
+			var div = document.getElementById("myImg");
 			var img = document.getElementById("myImg").getElementsByTagName('img');
 			var modalImg = document.getElementById("img01");
 			var captionText = document.getElementById("caption");
-			img.onclick = function(){
+			div.onclick = function(){
 			  modal.style.display = "block";
-			  modalImg.src = this.src;
-			  captionText.innerHTML = this.alt;
+			  modalImg.src = img.src;
+			  captionText.innerHTML = img.alt;
 			}
 
 		// Get the <span> element that closes the modal
