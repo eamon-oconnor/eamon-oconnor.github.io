01474debdf622d2a82b383df2ce7253f97900bab
ea-oconnor <oconnor.eamon0@gmail.com>
2024-09-29 12:00:13 -0400

    Update modal image js

--- assets/js/main.js
+++ assets/js/main.js
@@ -171,7 +171,7 @@
 		var modal = document.getElementById("myModal");
 
 		// Get the image and insert it inside the modal - use its "alt" text as a caption
-			var img = document.getElementById("#myImg img");
+			var img = document.getElementById("myImg").getElementsByTagName('img');;
 			var modalImg = document.getElementById("img01");
 			var captionText = document.getElementById("caption");
 			img.onclick = function(){
